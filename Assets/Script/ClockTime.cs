a5e9a09f63bacefc4dd3a7da73ea113f240deaae
ryosukeihara <work.ihara@gmail.com>
2014-06-25 01:17:27 +0900

    demo1

---
+++ Assets/Script/ClockTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ClockTime : MonoBehaviour {
+
+	[SerializeField]
+	private UILabel dateTimeLabel;
+
+	private DateTime dateTime;
+
+	
+	// Use this for initialization
+	void Start () {
+
+		dateTimeLabel = GameObject.Find("LabelDateTime").GetComponent<UILabel>();
+
+		dateTime = new DateTime();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		dateTimeLabel.text = DateTime.Now.ToString() +"  " ;
+
+	}
+}
